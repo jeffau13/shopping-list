d5521b01aab62c85734af1e0acb4481e64c345d1
jeffau13 <jeffau13@gmail.com>
2019-04-12 21:01:08 -0400

    created routes for backend API

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const DB = require('./config/keys').mongoURI;
+const items = require('./routes/api/items');
 
 const app = express();
 
@@ -11,6 +12,9 @@ mongoose
   .connect(DB, { useNewUrlParser: true })
   .then(() => console.log('MnogoDB connected'))
   .catch(err => console.log(err));
+
+//use items.js for routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on ${port} `));
