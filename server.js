60f76a6283f30c68b0c1203d34865cdf56d150e8
jeffau13 <jeffau13@gmail.com>
2019-04-12 20:34:29 -0400

    added db connection and model for mongo schema

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const DB = require('./config/keys').mongoURI;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+mongoose
+  .connect(DB, { useNewUrlParser: true })
+  .then(() => console.log('MnogoDB connected'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on ${port} `));
