4c8e5d4e79f938f0abf06f23a36875f964a7029b
jeffau13 <jeffau13@gmail.com>
2019-04-15 19:04:36 -0400

    created shoppingList component and csstransition

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <AppNavbar />;
+    return (
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    );
   }
 }
 
