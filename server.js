4e94258fce60296d4a2cbe4718f740698b33ca4c
jeffau13 <jeffau13@gmail.com>
2019-04-16 20:36:36 -0400

    version 1.0

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const DB = require('./config/keys').mongoURI;
 const items = require('./routes/api/items');
-
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
@@ -15,6 +15,15 @@ mongoose
 
 //use items.js for routes
 app.use('/api/items', items);
+
+//Serve static assets if env in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on ${port} `));
